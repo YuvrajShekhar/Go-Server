2e038e15c822e196a5af84a3e5bf69c108f1bca8
Yuvraj Shekhar <syuvraj1999@gmail.com>
2026-03-20 16:45:33 +0100

    succesfully added all the routes and called the functions as well

--- main.go
+++ main.go
@@ -6,10 +6,29 @@ import (
 	"net/http"
 )
 
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err : %v", err)
+		return
+	}
+	fmt.Fprintf(w, "Post request succesfull")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name=%s\n", name)
+	fmt.Fprintf(w, "Address=%s\n", address)
+
+}
+
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/hello" {
-		http.Error(w, "404 not found", http.St)
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
 	}
+	if r.Method != "GET" {
+		http.Error(w, "method is not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "hello!")
 }
 
 func main() {
